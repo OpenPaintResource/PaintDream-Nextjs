3eed3d7ce46d2853c37aafbeca40f776a4157431
Talbot3 <1796560392@qq.com>
2021-09-21 09:32:21 +0800

    fix: 更新备案号

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,10 +4,13 @@ import { AppProps } from 'next/app';
 import 'antd/dist/antd.css'
 import '../styles/vars.css'
 import '../styles/globals.css'
+import { CopyrightOutlined } from '@ant-design/icons';
+import GlobalFooter from '@/components/GlobalFooter';
 
 import {Layout} from 'antd'
 
 const {Header, Content, Footer} = Layout;
+const currentYear = new Date().getFullYear();
 
 export default function MyApp(props: AppProps) {
   const { Component, pageProps } = props;
@@ -30,6 +33,16 @@ export default function MyApp(props: AppProps) {
       </Layout>
       <Footer className="footer">
         <p>为独立游戏开发者提供免费素材,进行游戏的开发和学习,施工中</p>
+        <GlobalFooter
+          links={[]}
+          copyright={
+            <>
+              Copyright <CopyrightOutlined /> {currentYear} 绘旅书梦
+              <span style={{ padding: '0 20px' }}>|</span>
+              <a onClick={() => open('https://beian.miit.gov.cn')}>沪ICP备2021026434号</a>
+            </>
+          }
+        />
       </Footer>
     </React.Fragment>
   );
